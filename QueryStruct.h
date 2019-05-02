fe46ee91e923e7309b852a27c377bbca41a03985
Pavel <pavelbesedinsky@gmail.com>
2019-05-02 20:37:47 +0300

    Увеличил размер массива в структуре

--- QueryStruct.h
+++ QueryStruct.h
@@ -1,10 +1,11 @@
 #pragma once
 
-struct Query
+struct SQuery
 {
-	int _qID;			// Kind of query
+	int _qRequest;			// Kind of query
+	int _qClientID;		// Client ID
 	int _qSum;			// Sum of query
 	int _qResult;		// Result of query
-	char _qText[20];	// Text of answer
+	char _qText[64];	// Text of answer
 						// char '0' means end of the string
 };
