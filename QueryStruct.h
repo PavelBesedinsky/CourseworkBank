f10efaaafe475345ddedde224ff5f513852e1fd3
Pavel <pavelbesedinsky@gmail.com>
2019-05-03 20:49:51 +0300

    Вернул QueryStruct к предыдущему состоянию

--- QueryStruct.h
+++ QueryStruct.h
@@ -1,11 +1,10 @@
 #pragma once
-
 struct SQuery
 {
 	int _qRequest;			// Kind of query
 	int _qClientID;		// Client ID
 	int _qSum;			// Sum of query
 	int _qResult;		// Result of query
-	char _qText[64];	// Text of answer
+	char _qText[100];	// Text of answer
 						// char '0' means end of the string
 };
